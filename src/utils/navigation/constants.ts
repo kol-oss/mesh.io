18c52d5ae71e3839ae9fcdedb2d3e017b8045726
kol-oss <kolyakhozhajinov@gmail.com>
2026-04-28 00:22:11 +0300

    Replace view button with help

--- src/utils/navigation/constants.ts
+++ src/utils/navigation/constants.ts
@@ -3,4 +3,4 @@ import type { NavigationMenuItem } from "../../types/navigation";
 export const SIDEBAR_MIN_WIDTH_PERCENT = 20;
 export const SIDEBAR_MAX_WIDTH_PERCENT = 30;
 
-export const NAVIGATION_MENU_ITEMS: NavigationMenuItem[] = [{ title: "File" }, { title: "View" }];
+export const NAVIGATION_MENU_ITEMS: NavigationMenuItem[] = [{ title: "File" }, { title: "Help" }];
