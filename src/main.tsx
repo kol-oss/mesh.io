d0f63def2715b0bfa8128a03ec8aed51463aca74
kol-oss <kolyakhozhajinov@gmail.com>
2026-05-13 23:30:54 +0300

    Move main execution file

--- src/main.tsx
+++ src/main.tsx
@@ -1,1 +1,18 @@
-import "@/app/main";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "@/app/App";
+import { ReduxProvider } from "@/app/providers/ReduxProvider";
+import { BrowserRouter } from "react-router-dom";
+import { ToastProvider } from "@/app/providers/ToastProvider";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <ReduxProvider>
+      <BrowserRouter>
+        <ToastProvider>
+          <App />
+        </ToastProvider>
+      </BrowserRouter>
+    </ReduxProvider>
+  </StrictMode>,
+);
