b5034f44731cb5f11f91e73f0c4ee0c92598bba9
kol-oss <kolyakhozhajinov@gmail.com>
2026-06-02 23:32:59 +0300

    Fix route timeout validation

--- src/shared/constants/protocols/olsr.ts
+++ src/shared/constants/protocols/olsr.ts
@@ -3,7 +3,7 @@ export const OLSR_MIN_ROUTE_TIMEOUT = 2;
 export const OLSR_DEFAULT_TC_TTL = 16;
 
 export const OLSR_DEFAULT_CONFIGURATION = {
-  helloInterval: 2,
-  tcInterval: 5,
-  routeTimeout: 6,
+  helloInterval: 1,
+  tcInterval: 2,
+  routeTimeout: 5,
 };
