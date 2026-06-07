aaa27ad07e17effb54c53b48cef9a0d8ea7b7aaf
kol-oss <kolyakhozhajinov@gmail.com>
2026-06-07 17:41:21 +0300

    Add multipage support

--- src/shared/store/store.ts
+++ src/shared/store/store.ts
@@ -1,5 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import boardReducer from "@/shared/store/slices/boardSlice";
+import displayReducer, { replaceDisplay } from "@/shared/store/slices/displaySlice";
+import linkReducer, { replaceLinks } from "@/shared/store/slices/linkSlice";
+import obstacleReducer, { replaceObstacles } from "@/shared/store/slices/obstacleSlice";
+import peerReducer, { replacePeers } from "@/shared/store/slices/peerSlice";
+import simulationReducer from "@/shared/store/slices/simulationSlice";
+import stepReducer, { replaceSteps } from "@/shared/store/slices/stepSlice";
+import textReducer, { replaceTexts } from "@/shared/store/slices/textSlice";
+import { loadState, loadStates, setState } from "@/shared/store/utils/storeUtils";
+import type { LinkEntity, ObstacleEntity, PeerEntity } from "@/shared/types/model/entities";
+import type { Step } from "@/shared/types/model/steps";
+import type { TextItem } from "@/shared/types/workspace/text";
+import { sanitizePeerEntities } from "@/shared/utils/entities/sanitizers";
 import {
   DISPLAY_STORAGE_KEY,
   LINK_STORAGE_KEY,
@@ -8,15 +21,6 @@ import {
   STEP_STORAGE_KEY,
   TEXT_STORAGE_KEY,
 } from "./constants";
-import peerReducer from "@/shared/store/slices/peerSlice";
-import linkReducer from "@/shared/store/slices/linkSlice";
-import obstacleReducer from "@/shared/store/slices/obstacleSlice";
-import stepReducer from "@/shared/store/slices/stepSlice";
-import textReducer from "@/shared/store/slices/textSlice";
-import displayReducer from "@/shared/store/slices/displaySlice";
-import boardReducer from "@/shared/store/slices/boardSlice";
-import simulationReducer from "@/shared/store/slices/simulationSlice";
-import { setState } from "@/shared/store/utils/storeUtils";
 
 export const store = configureStore({
   reducer: {
@@ -45,3 +49,33 @@ store.subscribe(() => {
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+
+// Sync state changes made in other tabs into this tab's Redux store.
+window.addEventListener("storage", (event: StorageEvent) => {
+  if (!event.key || event.newValue === event.oldValue) return;
+
+  switch (event.key) {
+    case PEER_STORAGE_KEY:
+      store.dispatch(replacePeers(sanitizePeerEntities(loadStates<PeerEntity>(PEER_STORAGE_KEY))));
+      break;
+    case LINK_STORAGE_KEY:
+      store.dispatch(replaceLinks(loadStates<LinkEntity>(LINK_STORAGE_KEY)));
+      break;
+    case OBSTACLE_STORAGE_KEY:
+      store.dispatch(replaceObstacles(loadStates<ObstacleEntity>(OBSTACLE_STORAGE_KEY)));
+      break;
+    case STEP_STORAGE_KEY:
+      store.dispatch(replaceSteps(loadStates<Step>(STEP_STORAGE_KEY)));
+      break;
+    case TEXT_STORAGE_KEY:
+      store.dispatch(replaceTexts(loadStates<TextItem>(TEXT_STORAGE_KEY)));
+      break;
+    case DISPLAY_STORAGE_KEY: {
+      const display = loadState(DISPLAY_STORAGE_KEY, null);
+      if (display) store.dispatch(replaceDisplay(display));
+      break;
+    }
+    default:
+      break;
+  }
+});
